7315fa325c1018edb8829e1f5d1f1bdce6ae5fa3
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-06-05 15:53:41 -0700

    ✅ Pass tests

--- R/concatenate_maximum_counts.R
+++ R/concatenate_maximum_counts.R
@@ -1,6 +1,5 @@
-read_from_all_islets <- function(all_islets_path) {
+read_from_all_islets <- function(all_islets_path, not_wanted_islets = c("Alcatraz", "Bledos", "Pajaros", "Patos")) {
   data <- readr::read_csv(all_islets_path, show_col_types = FALSE)
-  not_wanted_islets <- c("Alcatraz", "Bledos")
   wanted_islet <- !data$Isla %in% not_wanted_islets
   data %>%
     dplyr::filter(wanted_islet)
