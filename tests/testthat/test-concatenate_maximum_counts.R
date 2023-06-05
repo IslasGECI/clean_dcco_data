667ed8683b3ebdf070e54e8156b237f0d2bacc3d
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-06-05 13:33:42 -0700

    🛑🧪 Fail tests

---
+++ tests/testthat/test-concatenate_maximum_counts.R
@@ -0,0 +1,24 @@
+describe("🪙 Concatenate conteo máximo de nidos por temporada con el conteo de todas las islas", {
+  it("concatenate_maximum_california_gulf", {
+    all_islets_path <- "/workdir/tests/data/conteo_nidos_cormoran_todas_islas.csv"
+    all_islets_data <- readr::read_csv(all_islets_path, show_col_types = FALSE)
+    california_path <- "/workdir/tests/data/conteo_alto_nidos_cormoran_golfo_california.csv"
+    california_data <- readr::read_csv(california_path, show_col_types = FALSE)
+    obtained <- concatenate_maximum_california_gulf(all_islets_data, california_data)
+    expected <- readr::read_csv("/workdir/tests/data/concatenated_california_data.csv", show_col_types = FALSE)
+    expect_equal(obtained, expected)
+  })
+})
+describe("Eliminar registros de islas en el golfo", {
+  it("Tirar registros por isla", {
+
+  })
+})
+describe("Agregar registros altos", {
+  it("Seleccionar nombre temporada", {
+
+  })
+  it("Concatenar registros altos", {
+
+  })
+})
