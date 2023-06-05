23c464abf8d04dcafa397067d5690a5b408d732d
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-06-05 15:39:19 -0700

    ♻️  Refactor

--- R/concatenate_maximum_counts.R
+++ R/concatenate_maximum_counts.R
@@ -1,6 +1,5 @@
 read_from_all_islets <- function(all_islets_path) {
   data <- readr::read_csv(all_islets_path, show_col_types = FALSE)
   data %>%
-    dplyr::filter(Isla != "Alcatraz") %>%
-    dplyr::filter(Isla != "Bledos")
+    dplyr::filter(Isla != all(c("Alcatraz", "Bledos")))
 }
