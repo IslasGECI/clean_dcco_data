94d56e2b1846546012a86fcdf3acefaf54d212cb
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-06-05 16:47:04 -0700

    ✅ Pass tests

--- R/concatenate_maximum_counts.R
+++ R/concatenate_maximum_counts.R
@@ -9,4 +9,8 @@ select_initial_year <- function(california_data) {
   california_data %>% tidyr::separate(Temporada, c("Temporada", NA), "/")
 }
 
-concatenate_california_and_all_islets <- function(california_data, all_islets_data) {}
+concatenate_california_and_all_islets <- function(california_data, all_islets_data) {
+  california_data %>%
+    dplyr::rename(Nidos_activos_por_visita = "Nidos_altos_por_temporada") %>%
+    rbind(all_islets_data)
+}
