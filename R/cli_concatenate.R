73957daef6ac348e39a09554140382af7fe2362a
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-08-18 13:02:55 -0700

    ✅ Pass tests

--- R/cli_concatenate.R
+++ R/cli_concatenate.R
@@ -11,3 +11,9 @@ select_pacific_islands_cli <- function(options) {
   pacific_islands <- select_pacific_islands(all_islets_data)
   readr::write_csv(pacific_islands, options[["output_path"]])
 }
+
+select_california_islands_cli <- function(options) {
+  all_islets_data <- readr::read_csv(options[["data_path"]], show_col_types = FALSE)
+  california_islands <- select_california_islands(all_islets_data)
+  readr::write_csv(california_islands, options[["output_path"]])
+}
