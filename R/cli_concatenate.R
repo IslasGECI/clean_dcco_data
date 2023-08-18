d1fcd8ff71e96cc2e746ea295580721e056dffac
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-08-18 11:24:20 -0700

    ✅ Pass tests

--- R/cli_concatenate.R
+++ R/cli_concatenate.R
@@ -5,3 +5,8 @@ cli_concatenate <- function(options) {
   obtained <- concatenate_maximum_california_gulf(all_islets_data, california_data)
   readr::write_csv(obtained, options[["output_path"]])
 }
+
+select_pacific_islands <- function(options) {
+  all_islets_data <- readr::read_csv(options[["data_path"]], show_col_types = FALSE)
+  readr::write_csv(all_islets_data, options[["output_path"]])
+}
