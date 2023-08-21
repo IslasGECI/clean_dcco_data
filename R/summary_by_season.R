63e22acdae5ef269a5cd9e4bac5491932485d910
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-08-21 09:37:14 -0700

    ✅ Pass tests

--- R/summary_by_season.R
+++ R/summary_by_season.R
@@ -1,5 +1,5 @@
 sum_by_season <- function(data_by_season_and_islet) {
   data_by_season_and_islet |>
     dplyr::group_by(Temporada) |>
-    dplyr::summarise(Nidos_activos_por_visita = sum(Nidos_activos_por_visita))
+    dplyr::summarise(Nidos_activos_por_visita = sum(Nidos_activos_por_visita, na.rm = TRUE))
 }
