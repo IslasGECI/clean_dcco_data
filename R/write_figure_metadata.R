ebcef41925a603e7241153518f5ac25fd16803c3
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-10-28 13:07:16 -0700

    ♻️  Refactor

--- R/write_figure_metadata.R
+++ R/write_figure_metadata.R
@@ -1,8 +1,12 @@
 construct_figure_metadata <- function(clean_data, island) {
-  first_season <- clean_data |>
-    rater::filter_by_island(island) |>
-    dplyr::pull(Temporada) |>
+  first_season <- extract_season_column_by_island(clean_data, island) |>
     min()
 
   list("island" = island, "figure_path" = NULL, "first_season" = first_season, "last_season" = NULL)
+}
+
+extract_season_column_by_island <- function(clean_data, island) {
+  clean_data |>
+    rater::filter_by_island(island) |>
+    dplyr::pull(Temporada)
 }
