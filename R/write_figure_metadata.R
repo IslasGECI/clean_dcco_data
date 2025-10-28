2802808a08eeb280938e6f956b493ef759cc32c0
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-10-28 12:43:57 -0700

    ✅ Pass tests

--- R/write_figure_metadata.R
+++ R/write_figure_metadata.R
@@ -1,3 +1,3 @@
 construct_figure_metadata <- function(clean_data, island) {
-  list("island" = NULL, "figure_path" = NULL, "first_season" = NULL, "last_season" = NULL)
+  list("island" = island, "figure_path" = NULL, "first_season" = NULL, "last_season" = NULL)
 }
