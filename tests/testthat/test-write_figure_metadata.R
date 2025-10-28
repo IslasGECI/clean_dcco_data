267506eb7cf6de095ec629cd5a437e9183f9aa49
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-10-28 12:39:21 -0700

    🛑🧪 Fail tests

--- tests/testthat/test-write_figure_metadata.R
+++ tests/testthat/test-write_figure_metadata.R
@@ -2,5 +2,8 @@ describe("write_figure_metadata", {
   clean_data_all_islets <- readr::read_csv("/workdir/tests/data/cormorant_all_islets_clean_data.csv")
   it("construct json", {
     obtained <- construct_figure_metadata(clean_data_all_islets, "Alcatraz")
+    expected_keys <- c("island", "figure_path", "first_season", "last_season")
+    obtained_keys <- names(obtained)
+    expect_equal(obtained_keys, expected_keys)
   })
 })
