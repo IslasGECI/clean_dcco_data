2829c9464dd95ca47d0a31feeed09db9adae073c
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2026-07-14 21:59:05 +0000

    🛑🧪 Fail tests

--- tests/testthat/test-compute_maximum_nests.R
+++ tests/testthat/test-compute_maximum_nests.R
@@ -1,6 +1,6 @@
 describe("Compute maximum nests from data for the same island and season", {
   it("First example", {
-    nests_counts <- readr::read_csv("tests/testthat/data/conteo_nidos_cormoran_todas_islas.csv")
+    nests_counts <- readr::read_csv("/workdir/tests/testthat/data/conteo_nidos_cormoran_todas_islas.csv")
     obtained <- compute_maximum_nests(nests_counts)
     expected_rows <- 31
     expect_equal(nrow(obtained), expected_rows)
