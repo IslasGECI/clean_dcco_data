0f9aa4f8dd6e9a0eed35af8130eb5862783ffafb
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2026-07-14 22:01:04 +0000

    🛑🧪 Fail tests

--- R/concatenate_maximum_counts.R
+++ R/concatenate_maximum_counts.R
@@ -1,3 +1,11 @@
+concatenate_maximum_california_gulf <- function(all_islets_data, california_data) {
+  no_california_islands <- all_islets_data |>
+    select_pacific_islands()
+  california_single_year_season <- california_data |>
+    select_initial_year()
+  concatenate_california_and_all_islets(california_single_year_season, no_california_islands)
+}
+
 select_pacific_islands <- function(data) {
   california_islands <- c("Alcatraz", "Bledos", "Pajaros", "Patos")
   drop_islands(data, california_islands)
@@ -35,11 +43,4 @@ concatenate_california_and_all_islets <- function(california_data, all_islets_da
     dplyr::rename(Nidos_activos_por_visita = "Nidos_altos_por_temporada") |>
     dplyr::select(-c("mas notas")) |>
     rbind(all_islets_data)
-}
-concatenate_maximum_california_gulf <- function(all_islets_data, california_data) {
-  no_california_islands <- all_islets_data |>
-    select_pacific_islands()
-  california_single_year_season <- california_data |>
-    select_initial_year()
-  concatenate_california_and_all_islets(california_single_year_season, no_california_islands)
 }
