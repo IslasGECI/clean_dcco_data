fa0cc5f91fa62480b9c9ec950f69264f7e5f8ae0
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2026-07-15 17:40:29 +0000

    🛑🧪 Fail tests

--- tests/testthat/test-compute_maximum_nests.R
+++ tests/testthat/test-compute_maximum_nests.R
@@ -8,7 +8,7 @@ describe("Compute maximum nests from data for the same island and season", {
     obtained_columns <- colnames(obtained)
     expected_column <- "Nidos_activos_por_visita"
     expect_true(expected_column %in% obtained_columns)
-    obtained_patos_2018_maximum <- obtained[[obtained$Isla == "Patos" & obtained$Temporada == 2018, expected_column]]
+    obtained_patos_2018_maximum <- obtained[obtained$Isla == "Patos" & obtained$Temporada == 2018, expected_column]
     expect_equal(obtained_patos_2018_maximum, 445)
   })
 })
