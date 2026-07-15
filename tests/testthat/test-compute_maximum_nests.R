e4369e728623a464b072215d97b83f8aab9fe6ce
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2026-07-15 17:44:30 +0000

    🛑🧪 Fail tests

--- tests/testthat/test-compute_maximum_nests.R
+++ tests/testthat/test-compute_maximum_nests.R
@@ -2,13 +2,12 @@ describe("Compute maximum nests from data for the same island and season", {
   it("First example", {
     nests_counts <- readr::read_csv("/workdir/tests/data/conteo_nidos_cormoran_todas_islas.csv")
     obtained <- compute_maximum_nests(nests_counts)
-    print(obtained)
     expected_rows <- 31
     expect_equal(nrow(obtained), expected_rows)
     obtained_columns <- colnames(obtained)
     expected_column <- "Nidos_activos_por_visita"
     expect_true(expected_column %in% obtained_columns)
-    obtained_patos_2018_maximum <- obtained[obtained$Isla == "Patos" & obtained$Temporada == 2018, expected_column]
+    obtained_patos_2018_maximum <- obtained$Nidos_activos_por_visita[obtained$Isla == "Patos" & obtained$Temporada == 2018]
     expect_equal(obtained_patos_2018_maximum, 445)
   })
 })
