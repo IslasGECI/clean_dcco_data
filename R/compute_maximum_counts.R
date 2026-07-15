d91a8a68e62fb46d080974995fa664bd62696045
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2026-07-15 17:51:09 +0000

    ✅ Pass tests

--- R/compute_maximum_counts.R
+++ R/compute_maximum_counts.R
@@ -1,5 +1,6 @@
 compute_maximum_nests <- function(nest_counts_df) {
   nest_counts_df |>
+    dplyr::filter(!is.na(Nidos_activos_por_visita)) |>
     dplyr::group_by(Isla, Temporada) |>
     dplyr::summarise(Nidos_activos_por_visita = max(Nidos_activos_por_visita, na.rm = TRUE))
 }
