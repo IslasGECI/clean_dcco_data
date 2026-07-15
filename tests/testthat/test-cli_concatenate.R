f08b5acaee65ce2de15f3e746dec1a7a1a0cc6b0
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2026-07-15 18:38:05 +0000

    ♻️ Refactor

--- tests/testthat/test-cli_concatenate.R
+++ tests/testthat/test-cli_concatenate.R
@@ -1,13 +1,14 @@
+concatenated_outuput_path <- "/workdir/tests/data/concatenated_output.csv"
 options <- list(
   right_data = "/workdir/tests/data/conteo_alto_nidos_cormoran_golfo_california.csv",
   left_data = "/workdir/tests/data/conteo_nidos_cormoran_todas_islas.csv",
-  output_path = "/workdir/tests/data/concatenated_output.csv"
+  output_path = concatenated_outuput_path
 )
 
 describe("🪙 Concatenate conteo máximo de nidos por temporada con el conteo de todas las islas", {
   it("concatenate_maximum_california_gulf", {
     cli_concatenate(options)
-    obtained <- readr::read_csv("/workdir/tests/data/concatenated_output.csv", col_types = "cccic", show_col_types = FALSE)
+    obtained <- readr::read_csv(concatenated_outuput_path, col_types = "cccic", show_col_types = FALSE)
     expected <- readr::read_csv("/workdir/tests/data/concatenated_california_data.csv", col_types = "cccic", show_col_types = FALSE)
     expect_equal(obtained, expected)
   })
@@ -16,13 +17,13 @@ describe("🪙 Concatenate conteo máximo de nidos por temporada con el conteo d
 describe("Drop pacific islands from csv with all islets", {
   data_without_pacific_islands <- "/workdir/tests/data/droped_pacific_islands.csv"
   options <- list(
-    data_path = "/workdir/tests/data/concatenated_output.csv",
+    data_path = concatenated_outuput_path,
     output_path = data_without_pacific_islands
   )
   it("select california gulf islands", {
     select_california_islands_cli(options)
     obtained <- readr::read_csv(data_without_pacific_islands, col_types = "cccic", show_col_types = FALSE)
-    original_data <- readr::read_csv("/workdir/tests/data/concatenated_output.csv", col_types = "cccic", show_col_types = FALSE)
+    original_data <- readr::read_csv(concatenated_outuput_path, col_types = "cccic", show_col_types = FALSE)
     obtained_length <- nrow(obtained)
     original_length <- nrow(original_data)
     expect_true(obtained_length < original_length)
@@ -32,13 +33,13 @@ describe("Drop pacific islands from csv with all islets", {
 describe("Drop gulf islands from csv with all islets", {
   data_without_gulf_islands <- "/workdir/tests/data/droped_gulf_islands.csv"
   options <- list(
-    data_path = "/workdir/tests/data/concatenated_output.csv",
+    data_path = concatenated_outuput_path,
     output_path = data_without_gulf_islands
   )
   it("select pacific gulf islands", {
     select_pacific_islands_cli(options)
     obtained <- readr::read_csv(data_without_gulf_islands, col_types = "cccic", show_col_types = FALSE)
-    original_data <- readr::read_csv("/workdir/tests/data/concatenated_output.csv", col_types = "cccic", show_col_types = FALSE)
+    original_data <- readr::read_csv(concatenated_outuput_path, col_types = "cccic", show_col_types = FALSE)
     obtained_length <- nrow(obtained)
     original_length <- nrow(original_data)
     expect_true(obtained_length < original_length)
