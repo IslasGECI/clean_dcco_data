73a02c089d78b438e84ce35c18b1e806bab49e38
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2026-07-15 21:40:33 +0000

    ♻️ Refactor 🆙 upgrade minor number version

--- tests/testthat/test_nothing.R
+++ tests/testthat/test_nothing.R
@@ -1,6 +1,6 @@
 describe("Get version of the module", {
-  it("The version is 0.4.0", {
-    expected_version <- c("0.4.0")
+  it("The version is 0.5.0", {
+    expected_version <- c("0.5.0")
     obtained_version <- packageVersion("cleandcco")
     version_are_equal <- expected_version == obtained_version
     expect_true(version_are_equal)
