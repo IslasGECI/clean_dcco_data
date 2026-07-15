eaa7916219e55db1ed86c5c96ca8f972abc82592
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2026-07-15 21:16:20 +0000

    🛑🧪 Fail tests

--- tests/testthat/test-domain_specific_options.R
+++ tests/testthat/test-domain_specific_options.R
@@ -1,7 +1,7 @@
 describe("get_domain_specific_options", {
   it("Defines domain specific options", {
     obtained_options <- get_domain_specific_options()
-    expected_options <- c("data-path", "island", "figure-path", "output-path")
+    expected_options <- c("data-path", "island", "figure-path", "output-path", "historical-data-path", "high-counts-path", "nests-counts-path")
     expect_true(all(expected_options %in% names(obtained_options)))
   })
 })
