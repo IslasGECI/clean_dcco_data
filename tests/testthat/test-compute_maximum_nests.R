e4d8ce69b33839cb06b7092a07ade36c846c4adc
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2026-07-21 17:19:27 +0000

    🛑🧪 Fail tests

--- tests/testthat/test-compute_maximum_nests.R
+++ tests/testthat/test-compute_maximum_nests.R
@@ -1,3 +1,23 @@
+describe("Compute maximum nests of two Islands", {
+  max_nest_count <- tibble::tibble(
+    Fecha = c("02/Nov/2020", "02/Nov/2021", "02/Nov/2020"),
+    Isla = c(rep("Bledos", 2), "Patos"),
+    Temporada = c("2020/2021", "2021/2022", "2020/2021"),
+    Nidos_altos_por_temporada = c(10, 20, 30)
+  )
+  nest_counts <- tibble::tibble(
+    Fecha = c("01/Nov/2019", "10/Dic/2021", "15/Dic/2021"),
+    Isla = c("Bledos", rep("Patos", 2)),
+    Temporada = c(2019, 2021, 2021),
+    Nidos_activos_por_visita = c(100, 200, 300)
+  )
+  obtained <- concatenate_two_islands(nest_counts, max_nest_count)
+  it("Assert shape", {
+    obtained_rows <- nrow(obtained)
+    expected_rows <- 6
+    expect_equal(obtained_rows, expected_rows)
+  })
+})
 describe("Compute maximum nests from data for the same island and season", {
   it("First example", {
     nests_counts <- readr::read_csv("/workdir/tests/data/conteo_nidos_cormoran_todas_islas.csv")
